aa982b3b655b1e5339538d278744ca8f9610654a
Sahandfer <karimiali0022@yahoo.com>
2020-12-07 12:32:34 +0800

    Updated concat.py

--- Scripts/concat.py
+++ Scripts/concat.py
@@ -1,32 +1,17 @@
-
+import glob
+import os
 import pandas as pd
 
+# path = 'training_data/2020_05_04/host/' # For host folder
 path = 'training_data/2020_05_04/trace/'
-a = pd.read_csv(path+'trace_csf.csv')
-b = pd.read_csv(path+'trace_fly_remote.csv')
-c = pd.read_csv(path+'trace_jdbc.csv')
-d = pd.read_csv(path+'trace_local.csv')
-e = pd.read_csv(path+'trace_osb.csv')
-f = pd.read_csv(path+'trace_remote_process.csv')
-# print(len(a)+len(b)+len(c)+len(d)+len(e)+len(f))
 
-# path = "sample/"
-# a = pd.read_csv(path+'CSF.csv')
-# b = pd.read_csv(path+'FlyRemote.csv')
-# c = pd.read_csv(path+'JDBC.csv')
-# d = pd.read_csv(path+'LOCAL.csv')
-# e = pd.read_csv(path+'OSB.csv')
-# f = pd.read_csv(path+'RemoteProcess.csv')
+csv_files = glob.glob(path+"*.csv")
 
-data = [a,b,c,d,e,f]
-data = pd.concat(data)
-data = data.sort_values(by=['startTime'])
-# print(len(data))
-data = data[:200000]
-data.to_csv(path+'trace_data_sample.csv')
-
-# data = pd.read_csv(path+'training_data/2020_05_04/trace/trace_data_sample.csv')
-
-# data['timeGroup'] = (data.startTime - data.startTime.iloc[0]) // 60000
-# for i in range(75):
-#     print(sum(data.timeGroup==i))
+dataset = []
+for file in csv_files:
+    content = pd.read_csv(file)
+    dataset.append(content)
+dataset = pd.concat(dataset, axis=0, ignore_index=True)
+# dataset = dataset.sort_values(by=['timestamp']) # For host folder
+dataset = dataset.sort_values(by=['startTime'])
+dataset.to_csv(path+"trace_data_full.csv")
