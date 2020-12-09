4d182124266c5eb12d2b248e822cf751a7a513f2
unknown <zhjhr181@gmail.com>
2020-12-09 14:36:22 +0800

    change hyperparam for trace anomaly

---
+++ run.sh
@@ -0,0 +1,1 @@
+python -m traceanomaly.main --trainpath ./train_ticket/train --normalpath ./train_ticket/test_normal --abnormalpath ./train_ticket/test_abnormal --outputpath result -c flow_type=rnvp
