14aa35b2f5f5e15eedea75f375e6601efb5fe235
Sahandfer <karimiali0022@yahoo.com>
2020-12-14 15:28:40 +0800

    Merge with master

--- Scripts/concat.py
+++ Scripts/concat.py
@@ -3,15 +3,18 @@ import os
 import pandas as pd
 
 # path = 'training_data/2020_05_04/host/' # For host folder
-path = 'training_data/2020_05_04/trace/'
+path = 'training_data/2020_04_11/host/'
 
 csv_files = glob.glob(path+"*.csv")
 
 dataset = []
 for file in csv_files:
     content = pd.read_csv(file)
+    content = content[content['timestamp'] < content['timestamp'][0]+100000]
     dataset.append(content)
+
 dataset = pd.concat(dataset, axis=0, ignore_index=True)
 # dataset = dataset.sort_values(by=['timestamp']) # For host folder
-dataset = dataset.sort_values(by=['startTime'])
-dataset.to_csv(path+"trace_data_full.csv")
+dataset = dataset.sort_values(by=['timestamp'])
+
+dataset.to_csv(path+"host_data_sample.csv")
--- run.sh
+++
@@ -1,1 +0,0 @@
-python -m trace.main --trainpath ./data/data_dict.json -c flow_type=rnvp
